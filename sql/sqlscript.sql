72a2f302413ccec9026228193a4fa16893af41ab
Mohita061 <76985832+Mohita061@users.noreply.github.com>
2021-03-15 19:04:08 +0530

    Add files via upload

---
+++ sql/sqlscript.sql
@@ -0,0 +1,17 @@
+REM   Script: ms_Activities
+REM   sql activitiees
+
+Create table Tablename;
+
+CREATE TABLE salesman ( 
+	 
+    salesman_id int, 
+	 
+    salesman_name varchar2(32), 
+	 
+    salesman_city varchar2(32), 
+	 
+    commission int 
+	 
+);
+
